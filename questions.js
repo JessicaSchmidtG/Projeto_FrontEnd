219cf217ee29f070e8a2e2beda8a89156fdabdbc
Jessica Schmidt <129232792+JessicaSchmidtG@users.noreply.github.com>
2023-10-24 21:52:17 -0300

    projeto

---
+++ questions.js
@@ -0,0 +1,62 @@
+let questions = [
+    {
+      question:
+        "Em qual elemento HTML nós colocamos o arquivo com a lógica javascript?",
+      options: ["tag js", "tag script", "tag scripting", "tag javascript"],
+      answer: 1,
+    },
+    {
+        question:
+          "Em qual elemento HTML nós colocamos o arquivo com a lógica javascript?",
+        options: ["tag js", "tag script", "tag scripting", "tag javascript"],
+        answer: 1,
+      },
+      {
+        question:
+          "Em qual elemento HTML nós colocamos o arquivo com a lógica javascript?",
+        options: ["tag js", "tag script", "tag scripting", "tag javascript"],
+        answer: 1,
+      },
+      {
+        question:
+          "Em qual elemento HTML nós colocamos o arquivo com a lógica javascript?",
+        options: ["tag js", "tag script", "tag scripting", "tag javascript"],
+        answer: 1,
+      },
+      {
+        question:
+          "Em qual elemento HTML nós colocamos o arquivo com a lógica javascript?",
+        options: ["tag js", "tag script", "tag scripting", "tag javascript"],
+        answer: 1,
+      },
+      {
+        question:
+          "Em qual elemento HTML nós colocamos o arquivo com a lógica javascript?",
+        options: ["tag js", "tag script", "tag scripting", "tag javascript"],
+        answer: 1,
+      },
+      {
+        question:
+          "Em qual elemento HTML nós colocamos o arquivo com a lógica javascript?",
+        options: ["tag js", "tag script", "tag scripting", "tag javascript"],
+        answer: 1,
+      },
+      {
+        question:
+          "Em qual elemento HTML nós colocamos o arquivo com a lógica javascript?",
+        options: ["tag js", "tag script", "tag scripting", "tag javascript"],
+        answer: 1,
+      },
+      {
+        question:
+          "Em qual elemento HTML nós colocamos o arquivo com a lógica javascript?",
+        options: ["tag js", "tag script", "tag scripting", "tag javascript"],
+        answer: 1,
+      },
+      {
+        question:
+          "Em qual elemento HTML nós colocamos o arquivo com a lógica javascript?",
+        options: ["tag js", "tag script", "tag scripting", "tag javascript"],
+        answer: 1,
+      },
+  ];
